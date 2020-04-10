131b86ad8396dcee52adba2fb6e58de5c0bb4a41
Mary Denysova <djailin.vasu@gmail.com>
2020-04-11 00:23:51 +0300

    Модификация запросов и обработка ответов

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,13 +1,20 @@
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpEventType } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 
 export class AuthInderceptor implements HttpInterceptor {
-
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Intercept request', req);
-    return next.handle(req);
-
+    const cloned = req.clone({
+      headers: req.headers.append('AuthInderceptorHeader','some-token')
+    })
+    console.log('Intercept request cloned', cloned);
+    return next.handle(cloned).pipe(
+      tap(event => {
+        if(event.type === HttpEventType.Response) {
+            console.log('Inderceptor Reaponse Event:', event)
+        }
+      })
+    );
   }
-
 }
