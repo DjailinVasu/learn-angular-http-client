819d30225778cd3caf91cbee84de40ce3d7ca23c
Mary Denysova <djailin.vasu@gmail.com>
2020-04-10 19:25:38 +0300

    Получение данных и вывод в шаблон

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface Todo {
+  completed: boolean,
+  title: string,
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular httpClient';
+
+  todos: Todo[] = []
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+    .subscribe(response => {
+      console.log(response);
+      this.todos = response;
+    })
+  }
 }
